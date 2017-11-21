100faa2cb69dc0ce0532ff8000da6f7d93261060
Robert <Box-Bop@github.com>
2017-11-21 16:59:42 +0200

    Initial commit

---
+++ NoteExcercise/Note.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteExcercise
+{
+    class Note
+    {
+        public string Author { get; set; }
+        public string NoteText { get; set; }
+        public DateTime ModificationDate { get; set; }
+
+        public void CreateNote()
+        {
+
+            ModificationDate = DateTime.Now;
+            Console.WriteLine("Author: " + Author + "\nNote: " + NoteText + "\nDate added: " + ModificationDate);
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ NoteExcercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Note note1 = new Note();
+            Console.WriteLine("Sisestage enda nimi: ");
+            note1.Author = Console.ReadLine();
+            Console.WriteLine("Sisestage märge: ");
+            note1.NoteText = Console.ReadLine();
+
+            note1.CreateNote();
+
+            Note note2 = new Note();
+            Console.WriteLine("Sisestage enda nimi: ");
+            note2.Author = Console.ReadLine();
+            Console.WriteLine("Sisestage märge: ");
+            note2.NoteText = Console.ReadLine();
+
+            note2.CreateNote();
+
+            Console.ReadLine();
+        }
+    }
+}
